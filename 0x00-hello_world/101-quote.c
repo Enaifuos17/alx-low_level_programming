55f2767ce5bc9aaadaa0bf409ff43f3daeb0814b
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-16 14:20:14 -0700

    Task 8 - print without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints some text without using printf or puts
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, sizeof(quote) / sizeof(char));
+	return (1);
+}

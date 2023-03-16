dda6960d2aa2de85800e593f452ebfd9e32a920f
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-16 11:04:06 -0700

    Task 6 - sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function for printing the size of various types
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %li byte(s)", sizeof(char);
+	printf("Size of an int: %li byte(s)", sizeof(int);
+	printf("Size of a long int: %li byte(s)", sizeof(long int);
+	printf("Size of a long long int: %li byte(s)", sizeof(long long char);
+	printf("Size of a float: %li byte(s)", sizeof(float);
+	return (0);
+}

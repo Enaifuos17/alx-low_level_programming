6d3ad6433eee725a8d7dd5cd3f87189feaeffb54
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-17 10:11:37 -0700

    Task 9 - Numbers separated by comma using only putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,15 @@
 int main(void)
 {
 	int i;
-	char comma = ',';
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 9)
-		{
-			putchar(48 + i);
-			break;
-		}
 		putchar(48 + i);
-		putchar(comma);
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

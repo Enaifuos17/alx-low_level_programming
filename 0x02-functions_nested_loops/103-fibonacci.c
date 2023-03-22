f22b298353554ad7b82f332e06848430b0e1d1b1
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-21 19:11:50 -0700

    Task 15 - fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fibonacci sequence
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum, res = 0;
+	unsigned long goal = 4000000;
+
+	for (count = 0; count < goal; count++)
+	{
+		sum = fib1 + fib2;
+		if (sum % 2 == 0)
+			res += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	return (0);
+}

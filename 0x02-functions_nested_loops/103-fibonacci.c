4134db830c98e0da6c3c9936f56719f73b0ac530
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-21 19:26:48 -0700

    Task 15 - fibonacci sequence

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,17 +9,23 @@
 
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, sum, res = 0;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long goal = 4000000, count;
+	float res;
 
-	for (count = 0; count < goal; count++)
+	while (1)
 	{
 		sum = fib1 + fib2;
-		if (sum % 2 == 0)
+
+		if (sum > goal)
+			break;
+
+		if ((sum % 2) == 2)
 			res += sum;
 
 		fib1 = fib2;
 		fib2 = sum;
 	}
+	printf("%0.f\n", res);
 	return (0);
 }

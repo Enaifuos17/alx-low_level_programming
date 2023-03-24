cefc24d48af37b3b329c006af03784d07d435552
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-24 06:43:00 -0700

    Task 11 - largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,64 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- * _sqrt - get the square of a number
- *
- * @x: number
- *
- * Return: 0
- */
-
-double _sqrt(double x)
-{
-	float sqrt, tmp;
-
-	sqrt = x / 2;
-	tmp = 0;
-
-	while (sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-
-	return (sqrt);
-}
-
-/**
- * largest_prime_factor - get the largest prime factor
- *
- * @num: number
- *
- * Return: 0
- */
-
-void largest_prime_factor(long long num)
-{
-	int primeNum, largest;
-
-	while (num % 2 == 0)
-		num = num / 2;
-	for (primeNum = 3; primeNum <= _sqrt(num); primeNum += 2)
-	{
-		while (num % primeNum == 0)
-		{
-			num = num / primeNum;
-			largest = primeNum;
-		}
-	}
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-/**
- * main - largest prime factor of a number
+ * main - prints the largest prime factor
  *
  * Return: 0
  */
 
 int main(void)
 {
-	largest_prime_factor(612852475143);
+	long int num, largest, primeNum;
+
+	num = 612852475143;
+
+	/* divide by the smallest prime number which is 2 */
+	while (num % 2 == 0)
+	{
+		largest = 2;
+		num = num / 2;
+	}
+	/* divide by the next prime number possible 3 (5 7 9...) */
+	for (primeNum = 3; primeNum <= sqrt(num); primeNum += 2)
+	{
+		while (num % primeNum == 0)
+		{
+			largest = primeNum;
+			num = num / primeNum;
+		}
+	}
+	if (num > 2)
+		largest = num;
+	printf("%ld\n", largest);
 	return (0);
 }

3bf411d8abb2cb1a167980e13c7410a54be07212
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-27 15:19:17 -0700

    Task 7 - prints half of as tring

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,18 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int i;
+	int i, start;
 
-	/* get the length */
+	/* length */
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		len++;
-	}
-	/* start from the half */
-	for (i = len / 2; str[i] != '\0'; i++)
-	{
+
+	start = len / 2;
+
+	if (len % 2 == 1)
+		start = ((len + 1) / 2);
+
+	for (i = start; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }

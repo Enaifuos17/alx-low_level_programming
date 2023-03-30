52ae9a13d8ee1cf0fed82bb34c33d7816bb629a0
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-30 02:01:59 -0700

    Task 8 - rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ *
+ * @str: string
+ *
+ * Return: string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}

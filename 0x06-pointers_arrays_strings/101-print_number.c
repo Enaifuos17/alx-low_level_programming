298afe716048aed7bdb18fe2027c36c07b2fcf86
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-03-30 07:32:25 -0700

    Task 9 - prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	int num;
+
+	num = n;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if ((num / 10) != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}

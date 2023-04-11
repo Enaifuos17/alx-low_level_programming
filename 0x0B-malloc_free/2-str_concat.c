1f856e4c20e4a14a33f9d9482c0330ca241d1b30
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-04-11 14:34:51 -0700

    Task 2 - concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	int i;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = "\0";
 
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
 		;

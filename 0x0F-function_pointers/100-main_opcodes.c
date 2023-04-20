24eb20cf81edca5a6bdad3f0cb858095515fc3e3
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-04-19 17:49:36 -0700

    Task 4 - opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -13,16 +12,16 @@
 int main(int argc, int **argv)
 {
 	char *p = (char *)main;
-	int b;
+	int bytes;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
 
-	b = atoi(argv[1]);
-	if (b < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 		printf("Error\n"), exit(2);
 
-	while (b--)
-		printf("%02hhx%s", *p++, b ? " " : "\n");
+	while (bytes--)
+		printf("%02hhx%s", *p++, bytes ? " " : "\n");
 	return (0);
 }

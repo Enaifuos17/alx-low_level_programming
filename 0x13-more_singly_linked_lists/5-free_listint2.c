07be11157fd352d9109125f587ba18c1c6cd000f
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-05-02 09:28:15 -0700

    Task 5 - Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ *
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	/* while node exist !== NULL */
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	/* sets the head to NULL */
+	*head = NULL;
+}

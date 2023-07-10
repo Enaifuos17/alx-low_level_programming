64358245a3d66cea5ace7625ac7a33e4c3bcf580
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-07-10 06:37:49 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#inlcude "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: file being read
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letter it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(o);
+
+	return (w);
+}

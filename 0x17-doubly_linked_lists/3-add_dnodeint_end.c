39b7008eac5ea6fc3a1462c9e76e6da1feea28fd
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-10-12 07:27:41 -0700

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,35 +1,55 @@
 #include "lists.h"
 
+dlistint_t *_add_dnodeint(dlistint_t **head, const int n);
 /**
- * add_dnodeint_end - adds new head
- * @head: address of pointer to current head node
- * @n: int field of new node
- *
- * Return: address of new node or NULL
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: double pointer to head of the list
+ * @n: value n of the new node to be added to the list
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint *new = malloc(sizeof(dlistint_t)), *node;
+	dlistint_t *new_node, *current_node;
 
-	if (!head || !new)
-		return (new ? free(new), NULL : NULL);
+	if (!(*head))
+		return (_add_dnodeint(head, n));
 
-	new->n = n;
-	new->next = NULL;
-	if (!*head)
-	{
-		new->prev = NULL;
-		*head = new;
-	}
-	else
-	{
-		node = *head;
-		while (node->next)
-		{
-			node = node->next
-		}
-		node->next = new;
-		new->prev = node;
-	}
-	return (new);
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	current_node = *head;
+
+	while (current_node->next)
+		current_node = current_node->next;
+
+	current_node->next = new_node;
+	new_node->prev = current_node;
+
+	return (new_node);
+}
+/**
+ * _add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: value n of the new node to be added to the list
+ * Return:  the address of the new element, or NULL if it failed
+ */
+dlistint_t *_add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+	return (new_node);
 }
